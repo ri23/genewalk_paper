84b4d7ee6d77d8d2ed886884fffd2da3d8e2d8ff
ri23 <robertietswaart@gmail.com>
2020-01-31 15:02:23 -0500

    REV6GRAN script

---
+++ GeneWalk_20200129REV6GRAN.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#Robert Ietswaart 
+#sbatch job to run GeneWalk for REVISIONS
+#adjusted from 20200123
+
+revv=REV6GRAN
+date=20200129${revv}
+script=genewalk/cli_${revv}.py 
+
+program=python
+notifEm=robert_ietswaart@hms.harvard.edu
+baseDir=/n/groups/churchman/ri23/genewalk
+scDir=/n/groups/churchman/ri23/code
+
+module load gcc/6.2.0
+module load python/3.6.0
+module load java/jdk-1.8u112
+
+cd ${scDir}
+mkdir -p ${baseDir}/LogErr
+
+data_source="indra pc" #
+Treatment="QKI JQ1" #
+nthread="1" #for stats 1 #all others: 8 
+SEEDS="42" #
+
+for seed in $SEEDS
+do
+    for ds in $data_source
+    do
+        for tr in $Treatment
+        do
+	    if [ ${tr} == 'QKI' ]
+	    then
+		idt=mgi_id
+		fnetwork=${tr}_MGIforINDRA_stmts.pkl
+	    else
+		idt=hgnc_id
+		fnetwork=${tr}_HGNCidForINDRA.pkl
+            fi
+	    stage=statistics #null_distribution  #all ##node_vectors
+        
+            jobname=GeneWalk_${date}_${tr}_${ds}_seed${seed}_stage${stage}
+            sbatch -p short -n ${nthread} --mem=8G -t 0-00:10:00 --job-name=${jobname} \
+                    -o ${baseDir}/LogErr/${jobname}.log -e ${baseDir}/LogErr/${jobname}.err \
+                    --mail-user=${notifEm} --mail-type=ALL \
+                    --wrap="source genewalkenv/bin/activate; \
+            cd ${scDir}/genewalk ; \
+            export PYTHONPATH=$PYTHONPATH:. ; \
+            $program ${script} --base_folder ${baseDir} --project ${date}_${tr}_${ds}seed${seed} \
+            --network_file ${baseDir}/${fnetwork} \
+            --network_source ${ds} --genes ${baseDir}/${tr}_forGW.csv --id_type ${idt} --stage ${stage} \
+            --nproc ${nthread} --nreps_graph 10 --nreps_null 10 --random_seed ${seed} ; \
+            pip freeze >> ${baseDir}/LogErr/${jobname}py.out"
+
+        done
+    done
+done
+
+#--mem=32G -t 0-11:59:00 short (all)
+#--mem=2G -t 0-00:10:00 short (statistics)
